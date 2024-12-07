2b7af30c17249f9884a792f186db1b7ef123a84d
mochrist <moritz.christ10@gmail.com>
2024-12-07 14:52:15 +0100

    Initial commit

---
+++ src/main/java/de/moch/webservertest/WebserverTestApplication.java
@@ -0,0 +1,13 @@
+package de.moch.webservertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserverTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebserverTestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/de/moch/webservertest/boundary/HelloController.java
@@ -0,0 +1,19 @@
+package de.moch.webservertest.boundary;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api")
+    public String sayHello() {
+        return "Hello World from the Backend";
+    }
+
+    @PostMapping("/api")
+    public String greetUser(String name) {
+        return "Hi " + name + ", how are you?";
+    }
+}
