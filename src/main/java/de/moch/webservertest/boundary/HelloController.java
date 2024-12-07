77a44c01f8baff1acfb8c22617cf25a9dc7d008c
mochrist <moritz.christ10@gmail.com>
2024-12-07 22:13:16 +0100

    Update HelloController

--- src/main/java/de/moch/webservertest/boundary/HelloController.java
+++ src/main/java/de/moch/webservertest/boundary/HelloController.java
@@ -7,13 +7,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/api")
-    public ResponseEntity<CustomMessage> sayHello() {
+    public ResponseEntity<List<CustomMessage>> sayHello() {
         CustomMessage response = new CustomMessage("Hello", "This is the default backend message");
-        return ResponseEntity.ok(response);
+        List<CustomMessage> messageList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            messageList.add(response);
+        }
+        return ResponseEntity.ok(messageList);
     }
 
     @PostMapping("/api")
