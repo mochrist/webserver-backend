2c080a111a9f3913a1101e28c638b57d38fa0f9f
mochrist <moritz.christ10@gmail.com>
2024-12-07 15:15:31 +0100

    Update HelloController

--- src/main/java/de/moch/webservertest/boundary/HelloController.java
+++ src/main/java/de/moch/webservertest/boundary/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("/api")
     public String sayHello() {
-        return "Hello World from the new Backend";
+        return "Hello World from the new Backend 2";
     }
 
     @PostMapping("/api")
