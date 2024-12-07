8fe45d9b18fb596e72d9cb4e648f39ff742f3799
mochrist <moritz.christ10@gmail.com>
2024-12-07 21:29:00 +0100

    Update HelloController

--- src/main/java/de/moch/webservertest/boundary/HelloController.java
+++ src/main/java/de/moch/webservertest/boundary/HelloController.java
@@ -2,6 +2,7 @@ package de.moch.webservertest.boundary;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +14,7 @@ public class HelloController {
     }
 
     @PostMapping("/api")
-    public String greetUser(String name) {
+    public String greetUser(@RequestBody String name) {
         return "Hi " + name + ", how are you?";
     }
 }
